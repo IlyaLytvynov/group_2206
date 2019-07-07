74373c7a66abe49ae936ce925e6d21927624a34f
Ilya Lytvynov <ilyaillych@gmail.com>
2019-07-07 14:36:20 +0300

    Added publish script

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,11 @@ const prodConfig = require('./config/prod');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-const env = process.env.NODE_ENV;
-
+const target = process.env.TARGET || 'build'
 const _path = __dirname;
 
 let config = baseConfig(_path);
 
-config = prodConfig(config, path.normalize(path.join(_path + "/build")));
+config = prodConfig(config, path.normalize(path.join(_path + `/${target}`)));
 
 module.exports = config;
